46370540aa552609ce299b0c62c0ad6e9cc3ab7d
Aprajita Srivastava <aprajita0625@gmail.com>
2024-10-15 17:47:39 -0400

    Connected the application to MongoBD

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+require('dotenv').config({path: './server/.env'});
+const uri = process.env.MONGO_URI;
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -7,4 +12,12 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
+})
+
+mongoose.
+connect(uri)
+.then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error)=>{
+  console.log(error)
 })
