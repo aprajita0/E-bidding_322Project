5f3a2e1e426acd6b490ea7e272d474372f98a705
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-10-28 14:36:18 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import './app.css';
 import Home from './frontend/pages/home';
-import U_login from './frontend/pages/user_login';
+import U_login from './frontend/pages/user_login';            
 import V_registration from './frontend/pages/visitor_registration';
+import trustsphere_logo from './frontend/assets/logo.png';
+import '@fontsource/dm-sans'; 
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
         <Router>
             <div>
                 <nav>
-                    <ul>
+                    <div className="logo">
+                        <img src={trustsphere_logo} alt="Logo" className="logo" />
+                    </div>
+                    <div className={`menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        {/* You can add an icon here for menu */}
+                        Menu
+                    </div>
+                    <ul className={`menu-list ${menuOpen ? 'open' : ''}`}>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
@@ -31,8 +45,8 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/U_login" element={<U_login />} />
-                    <Route path="/browse_listings" element={isLoggedIn ? <div>Browse</div> : <Navigate to="/U_login" />}/>
-                    <Route path="/my_account" element={isLoggedIn ? <div>My Account Page</div> : <Navigate to="/U_login" />}/>
+                    <Route path="/browse_listings" element={isLoggedIn ? <div>Browse</div> : <Navigate to="/U_login" />} />
+                    <Route path="/my_account" element={isLoggedIn ? <div>My Account Page</div> : <Navigate to="/U_login" />} />
                     <Route path="/V_registration" element={<V_registration />} />
                 </Routes>
             </div>
