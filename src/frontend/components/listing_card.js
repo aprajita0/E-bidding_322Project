8beba4452e61e9ba148452acbc757ddfc9c5d1df
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-11-14 19:29:44 -0500

    Update listing_card.js fixed it to add price range

--- src/frontend/components/listing_card.js
+++ src/frontend/components/listing_card.js
@@ -5,11 +5,19 @@ import './listing_card.css';
 const ListingCard = ({listing}) => {
     const navigate = useNavigate(); 
 
-    const formatAmount = (amount) => {
-        if (amount && typeof amount === 'object' && amount.$numberDecimal) {
-            amount = amount.$numberDecimal; 
+    const formatMin = (price_from) => {
+        if (price_from && typeof price_from === 'object' && price_from.$numberDecimal) {
+            price_from = price_from.$numberDecimal; 
         }
-        const number = Number(amount);
+        const number = Number(price_from);
+        return isNaN(number) ? "Not entered" : number.toFixed(2);
+    };
+
+    const formatMax = (price_to) => {
+        if (price_to && typeof price_to === 'object' && price_to.$numberDecimal) {
+            price_to = price_to.$numberDecimal; 
+        }
+        const number = Number(price_to);
         return isNaN(number) ? "Not entered" : number.toFixed(2);
     };
 
@@ -23,11 +31,12 @@ const ListingCard = ({listing}) => {
             <div className="card-name">{listing.name}</div>
             <div className="card-description">{listing.description}</div>
             <div className="card-label"> Type: {listing.type}</div>
-            <div className="card-label"> Price Range: ${formatAmount(listing.amount)}</div>
+            <div className="card-label"> Price Range: ${formatMin(listing.price_from)} - ${formatMax(listing.price_to)}</div>
             <div className="card-label"> Listed On: {new Date(listing.date_listed).toLocaleDateString()}</div>
         </div>
     );
 };
 
 export default ListingCard;
+
 
