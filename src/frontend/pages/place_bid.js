d386518516bca9bf3d8de17f084cf80016b49ceb
Jezlea Ortega <124209539+XJezleaX@users.noreply.github.com>
2024-11-22 11:18:18 -0500

    Empty place bid file for Tahsina to work with

---
+++ src/frontend/pages/place_bid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/place_bid.css';
+import { useNavigate } from 'react-router-dom';
+import '@fontsource/dm-sans/700.css'; 
+
+const Place_bid = () => {
+};
+
+export default Place_bid;
