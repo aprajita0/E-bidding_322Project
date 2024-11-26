231030760b7c28bcfb95c7af395b1e0d8ff8dba4
Aprajita Srivastava <98621004+aprajita0@users.noreply.github.com>
2024-11-25 20:13:21 -0500

    Update Transaction.js
    Updated transaction for pay fine

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const transactionSchema = new Schema({
     buyer_id: { type: Schema.Types.ObjectId, ref: 'User', required: true }, 
-    seller_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    listing_id: { type: Schema.Types.ObjectId, ref: 'Listing', required: true },
+    seller_id: { type: Schema.Types.ObjectId, ref: 'User'},
+    listing_id: { type: Schema.Types.ObjectId, ref: 'Listing'},
     amount: { type: mongoose.Types.Decimal128, required: true },
     transaction_date: { type: Date, default: Date.now }
   });
