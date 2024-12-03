658ec4f4815eb33f31f05f2d6f96b139b27b528d
tkhan015 <khantahsina015@gmail.com>
2024-12-03 14:36:08 -0500

    Fixed small errors in place_bid.js, place_bid.css from merge conflicts

--- src/frontend/pages/browse_listings.js
+++ src/frontend/pages/browse_listings.js
@@ -8,13 +8,6 @@ const Browse_listings = () => {
     const [listings, setListings] = useState([]);
     const [error, setError] = useState('');
     const navigate = useNavigate();
-<<<<<<< HEAD
-    /*const [accountBalance, setAccountBalance] = useState(0);
-    const [role, setRole] = useState('');*/
-=======
-    const [accountBalance, setAccountBalance] = useState(0);
-    const [role, setRole] = useState('');
->>>>>>> 98d38beb226dd8589d1a29c84f55ebdf8224ce36
 
     useEffect(() => {
         const fetchListings = async () => {
@@ -32,11 +25,7 @@ const Browse_listings = () => {
             }
         };
 
-<<<<<<< HEAD
         /*const fetchUserDetails = async (user_id) => {
-=======
-        const fetchUserDetails = async (user_id) => {
->>>>>>> 98d38beb226dd8589d1a29c84f55ebdf8224ce36
             try {
                 const token = localStorage.getItem('token');
                 const response = await fetch('/api/users/check-vip', {
@@ -45,11 +34,7 @@ const Browse_listings = () => {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${token}`,
                     },
-<<<<<<< HEAD
                     body: JSON.stringify({ user_id }), 
-=======
-                    body: JSON.stringify({ user_id }), // Send user_id in the request body
->>>>>>> 98d38beb226dd8589d1a29c84f55ebdf8224ce36
                 });
         
                 if (response.ok) {
@@ -71,28 +56,13 @@ const Browse_listings = () => {
             }
         };
         
-<<<<<<< HEAD
         fetchUserDetails();*/
-=======
-        fetchUserDetails();
->>>>>>> 98d38beb226dd8589d1a29c84f55ebdf8224ce36
         fetchListings();
     }, []);
 
     
-<<<<<<< HEAD
     const handleCardClick = (listing) => { 
         navigate(`/listings/${listing.id}`); 
-=======
-    const handleCardClick = (listing) => {
-        if (role === 'visitor') {
-            setError('Visitors cannot bid on items. Apply to be a user today.');
-        } else if (role === 'user' || role === 'superuser' || role === 'vip') {
-            navigate(`/listings/${listing.id}`);
-        } else {
-            setError('Unknown role. Please try again.');
-        }
->>>>>>> 98d38beb226dd8589d1a29c84f55ebdf8224ce36
     };
     
 
